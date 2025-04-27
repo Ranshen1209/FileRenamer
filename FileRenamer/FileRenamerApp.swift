ca2f880c3d34a1491eb81ba73df9e14d6c92c940
Jones <153377475+Ranshen1209@users.noreply.github.com>
2025-04-27 18:14:22 +0800

    Initial Commit

---
+++ FileRenamer/FileRenamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileRenamerApp.swift
+//  FileRenamer
+//
+//  Created by Ariel on 2025/4/27.
+//
+
+import SwiftUI
+
+@main
+struct FileRenamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
